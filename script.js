200f4d6ca42be17abe26da02dca7fcc55d72bb83
Saumya <saumyamanshrestha@gmail.com>
2023-03-26 12:46:26 +0545

    First commit

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 console.log("Javascript");
+
+// Alert dialog box
+// window.alert("Hello");
+
+// Confirm dialog box
+// let answer = window.confirm("Are you sure you want to exit?");
+// console.log(answer);
+
+// Prompt dialog box
+// let answer = window.prompt("What is your name?");
+//     if(answer == "Shreyash Sexit")
+//         window.alert("You are correct.");
+// console.log(answer);
+
+// Document
+// console.log(window.document.body);
+// console.log(window.document.body.style.backgroundColor = "red");
+// document.body.style.textAlign = "center";
+// document.body.innerHTML = "<h1> CSS </h1>"
+
+//Selector
+let movies = document.getElementById("movies");
+movies.style.backgroundColor = "cyan";
