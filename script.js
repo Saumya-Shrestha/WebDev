1416cc1f79016ea84521fbbc0a8b12dbca6f1f39
Saumya <saumyamanshrestha@gmail.com>
2023-03-30 08:06:27 +0545

    Last commit

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 let submitButton = document.getElementById("submitButton");
-submitButton.addEventListener("click", function() {
-    let input = document.getElementById("input").value;
-    let output = document.getElementById("output");
-    output.innerHTML = input;
+submitButton.addEventListener("click", function (e) {
+    e.preventDefault();
+    let signupForm = document.forms["signupForm"];
+    console.log(signupForm);
+    let firstNameInput = signupForm.firstName;
+    console.log(firstNameInput.value);
+    let lastNameInput = signupForm.lastName;
+    console.log(lastNameInput.value);
+    let emailInput = signupForm.email;
+    console.log(emailInput.value);
 });
